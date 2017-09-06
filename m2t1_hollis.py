e2725f8a3055bcc2e47a6b6d94f828fb99399d86
holliso <31700681+holliso@users.noreply.github.com>
2017-09-06 11:28:55 -0400

    Add files via upload
    uploaded for m2lab

---
+++ m2t1_hollis.py
@@ -0,0 +1,15 @@
+# cti 110
+# M2T1 - Sale Prediction
+# 8/30
+# hollis
+
+# suggested varibles: totalSales, annualProfit
+
+# ask the usetotal sales
+totalSales = int(input("Type totalSales: "))
+
+# caulate the profit amount
+annualProfit = 0.23 * totalSales
+
+#print the profit amount
+print(annualProfit)
